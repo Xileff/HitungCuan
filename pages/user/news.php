b95b4cea57b9d9920a6889854cbb8068f3e7990a
Felix <felixsavero78@gmail.com>
2022-04-25 16:26:11 +0700

    Refactored project structure

---
+++ pages/user/news.php
@@ -0,0 +1,172 @@
+<body>
+    <?php include 'pages/components/html-navbar.php'?>
+
+    <!-- Row Berita 1 -->
+    <div class="container container-news pt-5 mt-5 d-flex flex-column align-items-center">
+        <h2 class="text-center mb-4">Berita Cuan</h2>
+        <!-- row row-cols-1 row-cols-sm-2 row-cols-md-4 -->
+        <div class="row row-cols-2 row-cols-sm-2 row-cols-md-4 row-news">
+            <div class="col p-2 mx-auto">
+                <a href="newscontent.html">
+                    <div class="card card-news h-100">
+                        <img src="images/index/cryptocurrency-square.jpg" class="card-img-top" alt="cryptocurrency">
+                        <div class="card-body card-news-body w-100">
+                            <h5 class="card-title news-title general-link hvr-underline-from-left">Crypto Crash Lagi?</h5>
+                            <p class="card-text news-date">Senin, 7 Februari 2022</p>
+                        </div>
+                        <span class="card-news-author montserrat align-bottom px-3 pb-2">Author : HitungCuan</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col p-2 mx-auto">
+                <a href="#">
+                    <div class="card card-news h-100">
+                        <img src="images/index/graph-square.jpg" class="card-img-top" alt="graph">
+                        <div class="card-body card-news-body w-100">
+                            <h5 class="card-title news-title general-link hvr-underline-from-left">Binary Option : Judi Berkedok Trading</h5>
+                            <p class="card-text news-date">Senin, 7 Februari 2022</p>
+                        </div>
+                        <span class="card-news-author montserrat align-bottom px-3 pb-2">Author : HitungCuan</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col p-2 mx-auto">
+                <a href="#">
+                    <div class="card card-news h-100">
+                        <img src="images/index/empty-wallet-square.jpg" class="card-img-top" alt="dompet kosong">
+                        <div class="card-body card-news-body w-100">
+                            <h5 class="card-title news-title general-link hvr-underline-from-left">Aturan 50:30:20 untuk Keuangan</h5>
+                            <p class="card-text news-date">Senin, 7 Februari 2022</p>
+                        </div>
+                        <span class="card-news-author montserrat align-bottom px-3 pb-2">Author : HitungCuan</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col p-2 mx-auto">
+                <a href="#">
+                    <div class="card card-news h-100">
+                        <img src="images/index/NFT-square.jpg" class="card-img-top" alt="cryptocurrency">
+                        <div class="card-body card-news-body w-100">
+                            <h5 class="card-title news-title general-link hvr-underline-from-left">NFT Trending, Haruskah Beli Sekarang?</h5>
+                            <p class="card-text news-date">Senin, 7 Februari 2022</p>
+                        </div>
+                        <span class="card-news-author montserrat align-bottom px-3 pb-2">Author : HitungCuan</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Row Berita 2 -->
+    <div class="container container-news pt-2 d-flex flex-column align-items-center">
+        <!-- row row-cols-1 row-cols-sm-2 row-cols-md-4 -->
+        <div class="row row-cols-2 row-cols-sm-2 row-cols-md-4 row-news">
+            <div class="col p-2 mx-auto">
+                <a href="#">
+                    <div class="card card-news h-100">
+                        <img src="images/index/cryptocurrency-square.jpg" class="card-img-top" alt="cryptocurrency">
+                        <div class="card-body card-news-body w-100">
+                            <h5 class="card-title news-title general-link hvr-underline-from-left">Crypto Crash Lagi?</h5>
+                            <p class="card-text news-date">Senin, 7 Februari 2022</p>
+                        </div>
+                        <span class="card-news-author montserrat align-bottom px-3 pb-2">Author : HitungCuan</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col p-2 mx-auto">
+                <a href="#">
+                    <div class="card card-news h-100">
+                        <img src="images/index/graph-square.jpg" class="card-img-top" alt="graph">
+                        <div class="card-body card-news-body w-100">
+                            <h5 class="card-title news-title general-link hvr-underline-from-left">Binary Option : Judi Berkedok Trading</h5>
+                            <p class="card-text news-date">Senin, 7 Februari 2022</p>
+                        </div>
+                        <span class="card-news-author montserrat align-bottom px-3 pb-2">Author : HitungCuan</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col p-2 mx-auto">
+                <a href="#">
+                    <div class="card card-news h-100">
+                        <img src="images/index/empty-wallet-square.jpg" class="card-img-top" alt="dompet kosong">
+                        <div class="card-body card-news-body w-100">
+                            <h5 class="card-title news-title general-link hvr-underline-from-left">Aturan 50:30:20 untuk Keuangan</h5>
+                            <p class="card-text news-date">Senin, 7 Februari 2022</p>
+                        </div>
+                        <span class="card-news-author montserrat align-bottom px-3 pb-2">Author : HitungCuan</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col p-2 mx-auto">
+                <a href="#">
+                    <div class="card card-news h-100">
+                        <img src="images/index/NFT-square.jpg" class="card-img-top" alt="cryptocurrency">
+                        <div class="card-body card-news-body w-100">
+                            <h5 class="card-title news-title general-link hvr-underline-from-left">NFT Trending, Haruskah Beli Sekarang?</h5>
+                            <p class="card-text news-date">Senin, 7 Februari 2022</p>
+                        </div>
+                        <span class="card-news-author montserrat align-bottom px-3 pb-2">Author : HitungCuan</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Row Berita 3 -->
+    <div class="container container-news pt-2 d-flex flex-column align-items-center">
+        <!-- row row-cols-1 row-cols-sm-2 row-cols-md-4 -->
+        <div class="row row-cols-2 row-cols-sm-2 row-cols-md-4 row-news">
+            <div class="col p-2 mx-auto">
+                <a href="#">
+                    <div class="card card-news h-100">
+                        <img src="images/index/cryptocurrency-square.jpg" class="card-img-top" alt="cryptocurrency">
+                        <div class="card-body card-news-body w-100">
+                            <h5 class="card-title news-title general-link hvr-underline-from-left">Crypto Crash Lagi?</h5>
+                            <p class="card-text news-date">Senin, 7 Februari 2022</p>
+                        </div>
+                        <span class="card-news-author montserrat align-bottom px-3 pb-2">Author : HitungCuan</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col p-2 mx-auto">
+                <a href="#">
+                    <div class="card card-news h-100">
+                        <img src="images/index/graph-square.jpg" class="card-img-top" alt="graph">
+                        <div class="card-body card-news-body w-100">
+                            <h5 class="card-title news-title general-link hvr-underline-from-left">Binary Option : Judi Berkedok Trading</h5>
+                            <p class="card-text news-date">Senin, 7 Februari 2022</p>
+                        </div>
+                        <span class="card-news-author montserrat align-bottom px-3 pb-2">Author : HitungCuan</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col p-2 mx-auto">
+                <a href="#">
+                    <div class="card card-news h-100">
+                        <img src="images/index/empty-wallet-square.jpg" class="card-img-top" alt="dompet kosong">
+                        <div class="card-body card-news-body w-100">
+                            <h5 class="card-title news-title general-link hvr-underline-from-left">Aturan 50:30:20 untuk Keuangan</h5>
+                            <p class="card-text news-date">Senin, 7 Februari 2022</p>
+                        </div>
+                        <span class="card-news-author montserrat align-bottom px-3 pb-2">Author : HitungCuan</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col p-2 mx-auto">
+                <a href="#">
+                    <div class="card card-news h-100">
+                        <img src="images/index/NFT-square.jpg" class="card-img-top" alt="cryptocurrency">
+                        <div class="card-body card-news-body w-100">
+                            <h5 class="card-title news-title general-link hvr-underline-from-left">NFT Trending, Haruskah Beli Sekarang?</h5>
+                            <p class="card-text news-date">Senin, 7 Februari 2022</p>
+                        </div>
+                        <span class="card-news-author montserrat align-bottom px-3 pb-2">Author : HitungCuan</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    
+    <?php include 'pages/components/html-top.php'?>
+</body>
+</html>
