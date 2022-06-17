6edb6e481a4314be2e83c9cd4dc3145dc84a9730
Felix <felixsavero78@gmail.com>
2022-06-17 11:22:58 +0700

    Checkpoint : Before refactor to JSON

--- administrator/crud/addanswerquestions.php
+++ administrator/crud/addanswerquestions.php
@@ -1,21 +1,21 @@
-<?php 
+<?php
 global $conn;
 
-if(!isset($_GET['id'])){
-    alertRedirect('Error', 'Pertanyaan tidak ditemukan', '?page=questions&action=none','Ok');
+if (!isset($_GET['id'])) {
+    alertRedirect('Error', 'Pertanyaan tidak ditemukan', '?page=questions&action=none', 'Ok');
     return;
 }
 
 $idQuestion = $_GET['id'];
 $question = $conn->query("SELECT * FROM lessons_question WHERE id = $idQuestion")->fetch_assoc();
 
-if(!$question){
-    alertRedirect('Error', 'Pertanyaan tidak ditemukan', '?page=questions&action=none','Ok');
+if (!$question) {
+    alertRedirect('Error', 'Pertanyaan tidak ditemukan', '?page=questions&action=none', 'Ok');
     return;
 }
 
-if($question['answered']){
-    alertRedirect('Error', 'Pertanyaan sudah dijawab', '?page=questions&action=none','Ok');
+if ($question['answered']) {
+    alertRedirect('Error', 'Pertanyaan sudah dijawab', '?page=questions&action=none', 'Ok');
     return;
 }
 
@@ -24,42 +24,42 @@ $q = json_decode($jsonQuestion);
 
 
 // submit jawaban
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $answer = htmlspecialchars($_POST['text']);
     $idAdmin = $conn->query("SELECT id FROM admin WHERE username = '" . $_SESSION['username'] . "'")->fetch_assoc()['id'];
     $date = date('Y-m-d');
-    
+
     $conn->query("INSERT INTO lessons_question_answer VALUES('', '$idQuestion', '$idAdmin', '$date', '$answer')");
 
     // klo berhasil uplaod jawaban, update status pertanyaan mjd answered = true
-    if($conn->affected_rows !== 1){
+    if ($conn->affected_rows !== 1) {
         alertError('Gagal', 'Kesalahan server, silakan coba lagi', 'Ok');
         return;
     }
 
     $conn->query("UPDATE lessons_question SET answered = 1 WHERE id = $idQuestion");
 
-    if($conn->affected_rows !== 1){
+    if ($conn->affected_rows !== 1) {
         alertError('Gagal', 'Kesalahan server, silakan coba lagi', 'Ok');
         return;
     }
 
-    alertRedirect('Berhasil', 'Pertanyaan sudah dijawab', '?page=questions&action=none','Ok');
+    alertRedirect('Berhasil', 'Pertanyaan sudah dijawab', '?page=questions&action=none', 'Ok');
 }
 ?>
 <div class="container mt-5 pt-5">
-    <h1 class="font-green">Question <?=$q->id?></h1>
+    <h1 class="font-green">Question <?= $q->id ?></h1>
     <h3 class="text-secondary">Subject :
-        <?php 
-            $subject = $conn->query("SELECT nama_subject FROM subject WHERE id = (SELECT id_subject FROM lessons where id = " . $q->id_lesson .")")->fetch_assoc();
-            $jsonSubject = json_encode($subject);
-            $subject = json_decode($jsonSubject);
+        <?php
+        $subject = $conn->query("SELECT nama_subject FROM subject WHERE id = (SELECT id_subject FROM lessons where id = " . $q->id_lesson . ")")->fetch_assoc();
+        $jsonSubject = json_encode($subject);
+        $subject = json_decode($jsonSubject);
 
-            echo $subject->nama_subject;
+        echo $subject->nama_subject;
         ?>
     </h3>
     <br>
-    <p class="fs-4"><?=$q->teks?></p>
+    <p class="fs-4"><?= $q->teks ?></p>
 
     <form action="" method="post" class="d-flex flex-column">
         <div class="form-group mb-4">
@@ -67,14 +67,14 @@ if(isset($_POST['submit'])){
             <textarea id="text" type="text" name="text" class="form-control" rows="10" cols="50"></textarea>
         </div>
         <span class="montserrat text-warning align-self-end mb-1">Jawaban tidak dapat diubah atau dihapus</span>
-        <button class="btn btn-success rounded-5 align-self-end" name="submit"  id="btnSave" type="submit">Save</button>
+        <button class="btn btn-success rounded-5 align-self-end" name="submit" id="btnSave" type="submit">Save</button>
     </form>
 </div>
 <script>
     // cek input
     document.getElementById('btnSave').addEventListener('click', e => {
         const text = document.getElementById('text');
-        if(text.value.length < 1){
+        if (text.value.length < 1) {
             alertError('Jawaban kosong', 'Berikan jawaban terlebih dahulu sebelum submit', 'Ok');
             e.preventDefault();
             return;
--- virtualaccount.php
+++ virtualaccount.php
@@ -1,17 +1,17 @@
-<?php 
+<?php
 global $conn;
 
 // hideError();
 
 $validPmtMethod = ['bca', 'bri', 'gopay'];
-if(!in_array($_GET['payment'], $validPmtMethod)){
+if (!in_array($_GET['payment'], $validPmtMethod)) {
     // alertRedirect('Error', 'Tidak ada metode pembayaran tersebut', './', 'Ok');
     header("Location: ./");
     return;
 }
 
-$validPacket = [1,2,3];
-if(!in_array($_GET['idpacket'], $validPacket)){
+$validPacket = [1, 2, 3];
+if (!in_array($_GET['idpacket'], $validPacket)) {
     // alertRedirect('Error', 'Paket tidak ditemukan', './', 'Ok');
     header("Location: ./");
     return;
@@ -26,9 +26,9 @@ $payment = $_GET['payment'];
 $va = $conn->query("SELECT * FROM virtual_account WHERE id_user = $userId")->fetch_assoc();
 
 // cek apakah sudah expire
-if(date('Y-m-d') == $va['expire']){
+if (date('Y-m-d') == $va['expire']) {
     $conn->query("DELETE FROM virtual_account WHERE id_user = $userId");
-    if($conn->affected_rows !== 1){
+    if ($conn->affected_rows !== 1) {
         alertRedirect('Kesalahan server', 'Silakan coba lagi', './', 'Ok');
         return;
     }
@@ -36,16 +36,14 @@ if(date('Y-m-d') == $va['expire']){
 }
 
 // jika tidak ada/sudah expire, generate yg baru
-if(!$va) {
+if (!$va) {
     // $generatedVa = "8" . strval(mt_rand(1000000, 9999999));
     $generatedVa = "8" . strval(hexdec(uniqid()));
-    $conn->query("INSERT INTO virtual_account VALUES('$generatedVa', $userId, $idPacket, '$payment', " . $packet['harga'] .", '" . date("Y-m-d", strtotime("+1 days")) . "')");
+    $conn->query("INSERT INTO virtual_account VALUES('$generatedVa', $userId, $idPacket, '$payment', " . $packet['harga'] . ", '" . date("Y-m-d", strtotime("+1 days")) . "')");
 
-    if(mysqli_affected_rows($conn) === 1){
+    if (mysqli_affected_rows($conn) === 1) {
         $va = $conn->query("SELECT * FROM virtual_account WHERE id_user = $userId")->fetch_assoc();
-    }
-
-    else {
+    } else {
         alertRedirect('Error', 'Gagal menggenerate VA, silakan coba lagi', './', 'Ok');
         var_dump($conn->error);
         return;
@@ -53,11 +51,11 @@ if(!$va) {
 }
 
 
-if(isset($_POST['submit']) && $_POST['submit'] === 'pay'){
+if (isset($_POST['submit']) && $_POST['submit'] === 'pay') {
     // insert ke tabel subscription
     $today = date('Y-m-d');
     $expireDate;
-    switch($idPacket){
+    switch ($idPacket) {
         case 1:
             $expireDate = date('Y-m-d', strtotime('+ 365 days'));
             break;
@@ -67,34 +65,32 @@ if(isset($_POST['submit']) && $_POST['submit'] === 'pay'){
         case 3:
             $expireDate = date('Y-m-d', strtotime('+ 90 days'));
             break;
-        }
+    }
 
     $conn->query("DELETE FROM virtual_account WHERE id_user = $userId");
-    if($conn->affected_rows !== 1){
-        alertRedirect('Kesalahan server', 'Silakan coba lagi', $_SERVER['REQUEST_URI'] ,'Ok');
+    if ($conn->affected_rows !== 1) {
+        alertRedirect('Kesalahan server', 'Silakan coba lagi', $_SERVER['REQUEST_URI'], 'Ok');
         return;
     }
 
     $conn->query("INSERT INTO subscription VALUES('', $idPacket, $userId, '$expireDate')");
-    if($conn->affected_rows !== 1){
-        alertRedirect('Kesalahan server', 'Silakan coba lagi', $_SERVER['REQUEST_URI'] ,'Ok');
+    if ($conn->affected_rows !== 1) {
+        alertRedirect('Kesalahan server', 'Silakan coba lagi', $_SERVER['REQUEST_URI'], 'Ok');
         return;
     }
 
-    $conn->query("INSERT INTO revenue VALUES('', $idPacket, '$today', " . $packet['harga'] .")");
-    if($conn->affected_rows !== 1){
-        alertRedirect('Kesalahan server', 'Silakan coba lagi', $_SERVER['REQUEST_URI'] ,'Ok');
+    $conn->query("INSERT INTO revenue VALUES('', $idPacket, '$today', " . $packet['harga'] . ")");
+    if ($conn->affected_rows !== 1) {
+        alertRedirect('Kesalahan server', 'Silakan coba lagi', $_SERVER['REQUEST_URI'], 'Ok');
         return;
     }
-    alertRedirect('Berhasil', 'Anda sudah menjadi member, redirecting', './','Ok');
-}
-else if(isset($_POST['submit']) && $_POST['submit'] === 'cancel') {
+    alertRedirect('Berhasil', 'Anda sudah menjadi member, redirecting', './', 'Ok');
+} else if (isset($_POST['submit']) && $_POST['submit'] === 'cancel') {
     $conn->query("DELETE FROM virtual_account WHERE id_user = $userId");
-    if($conn->affected_rows === 1){
+    if ($conn->affected_rows === 1) {
         alertRedirect('Transaksi dibatalkan', 'Kembali ke halaman utama', './', 'Ok');
         return;
-    }
-    else {
+    } else {
         alertRedirect('Kesalahan server', 'Silakan coba lagi', './', 'Ok');
     }
 }
@@ -104,11 +100,11 @@ else if(isset($_POST['submit']) && $_POST['submit'] === 'cancel') {
     <section class="border-hitungcuan border-radius-10 p-5 mb-5">
         <form method="post">
             <p class="text-center fs-3">Virtual Account</p>
-            <p class="text-center fs-2 fw-bold font-green"><?=$va['id']?></p>
+            <p class="text-center fs-2 fw-bold font-green"><?= $va['id'] ?></p>
             <div class="d-flex justify-content-center mb-3">
-                <img src="assets/images/subscription/<?=$_GET['payment']?>.jpg" alt="gopay" class="img-fluid w-50 border-radius-10 mx-auto">
+                <img src="assets/images/subscription/<?= $_GET['payment'] ?>.jpg" alt="gopay" class="img-fluid w-50 border-radius-10 mx-auto">
             </div>
-            <p class="text-center fs-4 fw-bold">Bayar sebelum <?=tgl_indo($va['expire'])?></p>
+            <p class="text-center fs-4 fw-bold">Bayar sebelum <?= tgl_indo($va['expire']) ?></p>
             <p class="text-center fs-4">Sisa waktu pembayaran <span class="text-warning" id="spanCountdown"></span></p>
             <div class="d-flex flex-column justify-content-center">
                 <button type="submit" name="submit" class="index-headline-button mx-auto pt-2 pb-2 px-5 w-50 mb-2" style="background-color: rgb(117, 249, 145)" value="pay">Saya Sudah Bayar</button>
@@ -120,11 +116,12 @@ else if(isset($_POST['submit']) && $_POST['submit'] === 'cancel') {
 <div class="mb-5"></div>
 <script>
     const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"];
+        "July", "August", "September", "October", "November", "December"
+    ];
     const tomorrow = new Date(`${monthNames[new Date().getMonth()]} ${new Date().getDay()}, ${new Date().getFullYear()} 00:00:00`).getTime();
     const spanCountdown = document.getElementById('spanCountdown');
 
-    let x = setInterval(()=>{
+    let x = setInterval(() => {
         let now = new Date().getTime();
         let distance = tomorrow - now;
 
@@ -134,7 +131,7 @@ else if(isset($_POST['submit']) && $_POST['submit'] === 'cancel') {
 
         spanCountdown.innerHTML = (hours < 10 ? "0" + hours : hours) + " : " + (minutes < 10 ? "0" + minutes : minutes) + " : " + (seconds < 10 ? "0" + seconds : seconds);
 
-        if(distance < 0){
+        if (distance < 0) {
             clearInterval(x);
             spanCountdown.innerHTML = "EXPIRED";
         }
